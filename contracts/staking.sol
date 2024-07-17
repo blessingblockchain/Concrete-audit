f9aea45f627ac8067aab4f0a50b31ae649965839
blessingblockchain <adeolasola01@gmail.com>
2024-07-17 07:30:15 +0100

    refactored code & vault contract

---
+++ contracts/staking.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Vault} from "./Vault.sol";
+
+interface IERC20 {
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+}
+
+contract Staking {
+    struct Stake {
+        uint256 amount;
+        uint256 timestamp;
+    }
+
+    uint8 private _stakingDuration = 60 days;
+
+    IERC20 private _token;
+
+    Vault private _vault;
+
+    // artist => user => Stake tracker
+    mapping(address => mapping(address => Stake)) private _stakes;
+
+    // artist => totalStakes tracker
+    mapping(address => uint256) private _artistTotalStakes;
+
+    event StakeDeposited(
+        address indexed user,
+        address indexed artist,
+        uint256 amount
+    );
+
+    event StakeWithdrawn(
+        address indexed user,
+        address indexed artist,
+        uint256 amount
+    );
+
+    // Constructor
+
+    constructor(address _tokenAddress, address _vaultAddress) {
+        _token = IERC20(_tokenAddress);
+        
+        _vault = Vault(_vaultAddress);
+    }
+
+    // we need to ensure that address(this) has been set as a rewardDelegator in Vault so that users can earn rewards after staking for `stakingduration`
+    modifier isRewardDelegator() {
+        require(_vault._rewardDelegators[address(this)], "Deployer should set address(this) to be a reward delegator in Vault contract");
+
+        _;
+    }
+
+    function stakingDuration() public view returns(uint256) {
+        return _stakingDuration
+    }
+
+    function stakesInfo(address artist) public view returns (Stake) {
+        return _stakes[artist][msg.sender];
+    }
+
+    function artistTotalStakes(address artist) public view returns (uint256) {
+        return _artistTotalStakes[artist];
+    }
+
+    /**
+        @dev stakeToken
+        after msg.sender approves this contract to spend `amount` of their ERC20, calls this function to stake the approved `amount` of tokens
+    */
+    function stakeToken(address artist, uint256 amount) isRewardDelegator external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        require(
+            _token.transferFrom(msg.sender, address(this), amount),
+            "Amount not approved by msg.sender for contract to transfer"
+        );
+
+        _stakes[artist][msg.sender] = Stake(amount, block.timestamp);
+
+        emit StakeDeposited(msg.sender, artist, amount);
+    }
+
+    function withdrawToken(address artist) external {
+        Stake memory userStake = _stakes[artist][msg.sender];
+
+        require(userStake.amount > 0, "No stake found");
+
+        require(
+            block.timestamp >= userStake.timestamp + _stakingDuration,
+            "Stake is locked"
+        );
+
+        uint256 amount = userStake.amount;
+
+        _stakes[artist][msg.sender].amount = 0;
+
+        emit StakeWithdrawn(msg.sender, artist, amount);
+
+        require(_token.transfer(msg.sender, amount), "Token transfer failed");
+
+        // user earn 1% of staked amount as rewardToken
+        uint256 depositBasedReward = (10 * amount) / 100;
+
+        _vault.sendReward(msg.sender, depositBasedReward);
+    }
+}
---
+++ contracts/vault.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Governed} from "./Governed.sol";
+
+contract Vault is Governed {
+    mapping(addres => bool) private _rewardDelegators;
+
+    // reward token which vault would hold some balance of
+    IERC20 _token;
+
+    event RewardDelegatorAdded(address indexed account);
+    event RewardDelegatorRemoved(address indexed account);
+    event RewardTransfer(
+        address indexed to,
+        uint256 indexed amount,
+        address indexed delegator
+    );
+
+    constructor(address _rewardToken) {
+        _token = IERC20(_rewardToken);
+
+        _initialize(msg.sender);
+
+        _rewardDelegators[msg.sender] = true;
+    }
+
+    modifier onlyRewardDelegator() {
+        require(
+            _rewardDelegators[msg.sender],
+            "Only approved rewardDelegator can call this function"
+        );
+
+        _;
+    }
+
+    function addRewardDelegator(address _delegator) external onlyGovernor {
+        emit RewardDelegatorAdded(_delegator);
+
+        _rewardDelegators[_delegator] = true;
+    }
+
+    function removeRewardDelegator(address _delegator) external onlyGovernor {
+        emit RewardDelegatorRemoved(_delegator);
+
+        _rewardDelegators[_delegator] = false;
+    }
+
+    function sendReward(
+        address _recipient,
+        uint256 _amount
+    ) external onlyRewardDelegator {
+        require(
+            _token.balanceOf(address(this)) > _amount,
+            "Vault is drained. Fill-up to payout reward"
+        );
+
+        emit RewardTransfer(_recipient, _amount, msg.sender);
+
+        _token.transfer(_recipient, _amount);
+    }
+}
